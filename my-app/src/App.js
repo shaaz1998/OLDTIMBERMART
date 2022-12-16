0fdf0e3da20638faccb6f96dc0ecd82567490b16
Mohammed Shaaz <kmdshaaz8@gmail.com>
2022-12-16 14:48:25 +0530

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,8 @@ import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/About';
-import About from './components/pages/About';
+import Footer from './components/Footer';
+// import About from './components/pages/About';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={About} />
+          <Route path='/Footer' component={Footer} />
         </Switch>
       </Router>
     </>
